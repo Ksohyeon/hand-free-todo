d2515f1afa43785a5a8e3251f06f7e292cfad167
ksohyeon0302 <ksohyeon0302@gmail.com>
2024-04-05 22:05:03 +0900

    conflicts fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,6 @@
 import "@/styles/globals.css";
-import Footer from "@/src/component/Footer";
-import Header from "@/src/component/Header";
 import type { AppProps } from "next/app";
-import { ReduxProviders } from "../src/redux/reduxProvider";
-import ThemeWrapper from "./ThemeWrapper";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <ReduxProviders>
-        <ThemeWrapper>
-          <Header />
-          <Component {...pageProps} />
-          <Footer />
-        </ThemeWrapper>
-      </ReduxProviders>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- src/redux/reduxProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { store } from "./store";
-import { Provider } from "react-redux";
-
-export function ReduxProviders({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
